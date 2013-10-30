b93847032e4e4a4b726279e5ebdd19e62b50c7ea
xie <izzy.xie@gmail.com>
2013-10-30 12:19:32 -0400

    my first commit

---
+++ src/EmailSender.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.net.*;
+
+public class EmailSender
+{
+   public static void main(String[] args) throws Exception
+   {
+      // Establish a TCP connection with the mail server.
+      Socket socket = new Socket("MX.nyu.edu", 25);
+
+      // Create a BufferedReader to read a line at a time.
+      InputStream is = socket.getInputStream();
+      InputStreamReader isr = new InputStreamReader(is);
+      BufferedReader br = new BufferedReader(isr);
+
+      // Read greeting from the server.
+      String response = br.readLine();
+      System.out.println(response);
+      if (!response.startsWith("220")) {
+         throw new Exception("220 reply not received from server.");
+      }
+
+      // Get a reference to the socket's output stream.
+      OutputStream os = socket.getOutputStream();
+
+      // Send HELO command and get server response.
+      String command = "HELO x\r\n";
+      System.out.print(command);
+      os.write(command.getBytes("US-ASCII"));
+      response = br.readLine();
+      System.out.println(response);
+      if (!response.startsWith("250")) {
+         throw new Exception("250 reply not received from server.");
+      }
+
+      // Send MAIL FROM command.
+      command = "MAIL FROM: xie@bouncex.com\r\n";
+      System.out.print(command);
+      os.write(command.getBytes("US-ASCII"));
+      response = br.readLine();
+      System.out.println(response);
+      if (!response.startsWith("250")) {
+         throw new Exception("250 reply not received from server.");
+      }
+
+      // Send RCPT TO command.
+      command = "RCPT TO: hx324@nyu.edu\r\n";
+      System.out.print(command);
+      os.write(command.getBytes("US-ASCII"));
+      response = br.readLine();
+      System.out.println(response);
+      if (!response.startsWith("250")) {
+         throw new Exception("250 reply not received from server.");
+      }
+
+      // Send DATA command.
+      command = "DATA\r\n";
+      System.out.print(command);
+      os.write(command.getBytes("US-ASCII"));
+      response = br.readLine();
+      System.out.println(response);
+      if (!response.startsWith("354")) {
+         throw new Exception("354 reply not received from server.");
+      }
+
+      // Send message data.
+      os.write("SUBJECT: Computer Networking 2013Fall SMTP test message\r\n\r\n".getBytes("US-ASCII"));
+      os.write("Hi there,\r\n".getBytes("US-ASCII"));
+      os.write("\r\n".getBytes("US-ASCII"));
+      os.write("This is CS6083 Programming homework 2 : SMTP.\r\n".getBytes("US-ASCII"));
+
+      // End with line with a single period.
+      os.write(".\r\n".getBytes("US-ASCII"));
+      response = br.readLine();
+      System.out.println(response);
+      if (!response.startsWith("250")) {
+         throw new Exception("250 reply not received from server.");
+      }
+
+      // Send QUIT command.
+      command = "QUIT\r\n";
+      System.out.print(command);
+      os.write(command.getBytes("US-ASCII"));
+      response = br.readLine();
+      System.out.println(response);
+   }
+}
